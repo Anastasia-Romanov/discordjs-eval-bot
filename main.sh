93d5bfa21359df5c11432d1b15fdc78887ae5809
Anastasia Romanov <86816930+Anastasia-Romanov@users.noreply.github.com>
2021-07-27 04:03:07 +0000

    test modding remote

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@ while [ true ]; do
 # Git reset for security
 rm -rf .git
 git init -q
-git remote add origin https://github.com/doinkythederp/discordjs-eval-bot.git
+git remote add origin "$REMOTE_URL"
 git fetch -q
 git reset -q origin/master --hard
 sleep 3s;
